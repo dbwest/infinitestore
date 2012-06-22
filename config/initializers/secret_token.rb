b3d4509ec85cf349b2fb325bdcc67626e6754175
David West <david.b.west@gmail.com>
2012-06-22 12:22:04 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dailythreads::Application.config.secret_token = '7cc394aa06cbe401f7cbfaf24389731cf417ed4d540f3b794615b4a823ebd90a76d6ae398d99039f62e8f7789bb743902192598b4efca30fb3c445d99b3a6d90'
