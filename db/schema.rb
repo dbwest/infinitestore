06b1cfcae2c8a90a751145d16e055677d6786c6b
David West <david.b.west@gmail.com>
2012-06-22 14:53:15 -0400

    time to make things look nice first let's make an index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120622172125) do
+ActiveRecord::Schema.define(:version => 20120622184343) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120622172125) do
     t.decimal  "price"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "category"
   end
 
 end
