f89101bd4ea5b4ca8f7052b43bf6f8f29263a1a2
David West <david.b.west@gmail.com>
2012-06-24 16:38:18 -0400

    changed problem

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
+  current_item = line_items.find_by_product_id(product_id)
   has_many :line_items, dependent: :destroy
   if current_item
   	current_item.quantity += 1
