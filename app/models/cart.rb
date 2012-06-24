a322c4efc2d7c0a8ec00b15d395167595bd043a4
David West <david.b.west@gmail.com>
2012-06-24 16:34:02 -0400

    changes to push migrate too

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, dependent: :destroy
+  if current_item
+  	current_item.quantity += 1
+  else
+  	current_item = line_items.products.build(product_id: product_id)
+  end
+  current_item
 end
