d5a7884f45527f36d591f60a90e25878b7d34830
David West <david.b.west@gmail.com>
2012-06-25 12:15:23 -0400

    new row every 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def new_row_every_3
+	count = 3
+    if (count%3==0)
+      attributes["class"] = "row"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,9 @@
 module StoreHelper
+	def new_row_every_3 (&block)
+		attributes = {}
+		count = 3
+	    if (count%3==0)
+		    content_tag("div", :class => "row", &block)
+		end
+    end
 end
