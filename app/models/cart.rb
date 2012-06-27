af6c9ff1f4f7a070fd91f551979141bd3c0d4862
David West <david.b.west@gmail.com>
2012-06-27 14:56:47 -0400

    let's go

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,6 +10,7 @@ class Cart < ActiveRecord::Base
   attr_accessible :product_id, :product
   has_many :line_items, dependent: :destroy
 
+
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
     if current_item
@@ -23,5 +24,6 @@ class Cart < ActiveRecord::Base
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
+
 end
 
