47e10628e067665ce3ba99048bee7e76e36da981
David West <david.b.west@gmail.com>
2012-07-03 15:30:08 -0400

    just adding some changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by_name(params[:name])
+  	if user and user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to admin_url
+  	else
+  		redirect_to login_url, alert "Invalid Username or pass"
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to store_url, notice: "Logged out"
   end
 end
